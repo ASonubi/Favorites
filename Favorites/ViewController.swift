285b1d9aec285974b7744bc2aca6268436d6934b
Akinkunmi Sonubi <jkumkum13@gmail.com>
2021-11-01 12:18:36 -0400

    Don't

--- Favorites/ViewController.swift
+++ Favorites/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var images:[UIImage] = [#imageLiteral(resourceName: "JohnWick"), #imageLiteral(resourceName: "Darkness"), #imageLiteral(resourceName: "Endgame"), #imageLiteral(resourceName: "Naruto"), #imageLiteral(resourceName: "IMBD")]
+        
+        var heading:[String] = ["Darkness", "Endgame", "IMBD", "JohnWick", "Naruto"]
+        
+        
+        var captions = []
+        
     }
 
 
