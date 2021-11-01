603f89536a6e23c392b54b2efda93ce8c230331d
Akinkunmi Sonubi <jkumkum13@gmail.com>
2021-11-01 11:11:11 -0400

    Initial Commit

---
+++ Favorites/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Favorites
+//
+//  Created by Akinkunmi Sonubi on 11/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
